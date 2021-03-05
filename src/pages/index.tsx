f8736c9b23399c854e7c1378fa3e173795e5ca01
David Rock <davidrocha@outlook.com>
2021-03-05 18:59:12 -0300

    add title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,14 @@ import { Countdown } from './components/countdown/Countdown';
 import { ExperienceBar } from './components/experience-bar/ExperienceBar';
 import { Profile } from './components/profile/Profile';
 
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <div className="container flex flex-col px-8 pt-10 mx-auto my-0">
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
       <ExperienceBar />
       <section className="grid content-center h-screen grid-cols-2 gap-24">
         <div className="">
